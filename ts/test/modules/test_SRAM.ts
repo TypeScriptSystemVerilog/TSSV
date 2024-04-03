7cf784ace5190ee4e9a0429cadf10d8cfe961b5e
aigcic-lihongyi <hongyi.li@aigcic.com>
2024-04-03 18:45:47 +0800

    SRAM update, modify function of writeEnableMask and readBehavior

--- ts/test/modules/test_SRAM.ts
+++ ts/test/modules/test_SRAM.ts
@@ -1,21 +1,21 @@
 import { SRAM } from 'tssv/lib/modules/SRAM'
 import { writeFileSync } from 'fs'
 
-const testMem_1rw = new SRAM({ dataWidth: 8, depth: 32n, ports: '1rw', writeEnableMask: 'bit' })
+const testMem_1rw = new SRAM({ dataWidth: 8, depth: 32n, ports: '1rw', writeEnableMask: 'bit', readBehavior: 'undefOnWrite' })
 try {
   writeFileSync(`sv-examples/${testMem_1rw.name}.sv`, testMem_1rw.writeSystemVerilog())
 } catch (err) {
   console.error(err)
 }
 
-const testMem_2rw = new SRAM({ dataWidth: 8, depth: 32n, ports: '2rw', writeEnableMask: 'bit' })
+const testMem_2rw = new SRAM({ dataWidth:8, depth: 32n, ports: '2rw', writeEnableMask: 'bit', readBehavior: 'undefOnWrite' })
 try {
   writeFileSync(`sv-examples/${testMem_2rw.name}.sv`, testMem_2rw.writeSystemVerilog())
 } catch (err) {
   console.error(err)
 }
 
-const testMem_1r_1w = new SRAM({ dataWidth: 32, depth: 256n, ports: '1r_1w', writeEnableMask: 'byte' })
+const testMem_1r_1w = new SRAM({ dataWidth:32, depth: 256n, ports: '1r_1w', writeEnableMask: 'byte', readBehavior: 'readBeforeWrite' })
 try {
   writeFileSync(`sv-examples/${testMem_1r_1w.name}.sv`, testMem_1r_1w.writeSystemVerilog())
 } catch (err) {
