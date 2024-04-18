3cd50e8cbf780f31c3215d3e2b90cc328afe6d26
aigcic-lihongyi <hongyi.li@aigcic.com>
2024-04-18 12:51:13 +0800

    rom update, add endianness function

--- ts/src/modules/ROM.ts
+++ ts/src/modules/ROM.ts
@@ -9,38 +9,132 @@ export interface ROM_Parameters extends TSSVParameters {
      * bit width of ROM data
      */
   dataWidth: IntRange<1, 256>
+  /**
+     * endianness of ROM data
+     */
+  endianness?: 'big' | 'little'
 }
 export class ROM extends Module {
   declare params: ROM_Parameters
-  declare MemInitFile: string | string[]// initial data file path or array
+  declare MemInitFile // initial data file path or array
   declare rcf_path?: string // rcf file path
-  constructor (params: ROM_Parameters, MemInitFile: string | string[], rcf_path?: string) {
+  constructor (params: ROM_Parameters, MemInitFile: string | number[] | Uint8Array, rcf_path?: string) {
     super({
       // define the default parameter values
       name: params.name,
-      dataWidth: params.dataWidth
+      dataWidth: params.dataWidth,
+      endianness: params.endianness || 'big'
     })
     this.rcf_path = rcf_path || ''
     this.MemInitFile = MemInitFile
 
+    // Extract a string by identifier and convert it to hexadecimal, returning an array in hexadecimal
+    function getStringsAfterChars (strings: string[] | number[] | Uint8Array, data_width: number): string[] {
+      // If the input is of type Uint8Array, first convert it to a string array
+      let strings_array: string[] = []
+      if (strings instanceof Uint8Array) {
+        strings_array = strings.join(' ').split(' ')
+      } else {
+        strings_array = strings.map(num => num.toString()) // Convert number array to string array
+      }
+      return strings_array.map(str => {
+        let result_temp = str
+        let result = ''
+        let index_hex: number = -1
+        for (const i of ['0x', '0X', '\'h']) {
+          index_hex = result_temp.indexOf(i)
+          if (index_hex !== -1) break
+        }
+        const index_dec = result_temp.indexOf('\'d')
+        const index_bin = result_temp.indexOf('\'b')
+        if (index_hex !== -1) { // Hexadecimal, output directly after truncation
+          result += result_temp.substring(index_hex + '0x'.length)
+        } else if (index_bin !== -1) { // Binary, truncated and converted to hexadecimal output
+          result_temp = result_temp.substring(index_bin + '\'b'.length)
+          const str2num = parseInt(result_temp, 2) // string to number
+          result += str2num.toString(16).toUpperCase() // dec to hex
+        } else if (index_dec !== -1) { // Decimal, truncated to determine positive or negative and convert to hexadecimal output
+          result_temp = result_temp.substring(index_dec + '\'d'.length)
+          if (str.startsWith('-')) { // Determine whether the decimal number is negative, if it is negative, take the inverse and add one to its absolute value
+            const abs_dec2bin = (Number(result_temp).toString(2)).padStart(data_width, '0')
+            const bin_inv = abs_dec2bin.split('').map(char => char === '0' ? '1' : '0').join('')
+            const inv_add1 = (parseInt(bin_inv, 2) + 1).toString(2)
+            result += parseInt(inv_add1, 2).toString(16).toUpperCase()
+          } else {
+            result += Number(result_temp).toString(16).toUpperCase()
+          }
+        } else { // If there is no identifier, default to decimal number
+          if (str.startsWith('-')) { // Determine whether the decimal number is negative, if it is negative, take the inverse and add one to its absolute value
+            result_temp = result_temp.substring(1)
+            const abs_dec2bin = (Number(result_temp).toString(2)).padStart(data_width, '0')
+            const bin_inv = abs_dec2bin.split('').map(char => char === '0' ? '1' : '0').join('')
+            const inv_add1 = (parseInt(bin_inv, 2) + 1).toString(2)
+            result += parseInt(inv_add1, 2).toString(16).toUpperCase()
+          } else {
+            result += Number(result_temp).toString(16).toUpperCase()
+          }
+        }
+        // initial file Data bit width check
+        if (result.length * 4 > data_width) throw Error(`The bit width of the initial data '${result}'is greater than the set width '${data_width}'`)
+        return result
+      })
+    }
+
+    // This function determines whether it is in endian mode based on the initial input data type and the set ROM bit width
+    function EndianModeCheck (arrays: string[] | number[] | Uint8Array, data_width: number): number {
+      let endian_mode = 0
+      if (Array.isArray(arrays) && arrays.every((item: any) => typeof item === 'string')) { // The input array comes from the initialization file
+        const char_index = (arrays[0] as string).indexOf('\'b')
+        const isBinary = (char_index !== -1) // Check if the data is binary
+        if (isBinary) {
+          // Check if the data bit width is 8 and if the ROM bit width is an integer multiple of the data bit width
+          const width_multOf8 = ((((arrays[0] as string).substring(char_index + '\'b'.length)).length === 8) && (data_width % 8 === 0) && (data_width / 8 > 1))
+          if (width_multOf8) {
+            endian_mode = 1
+          }
+        }
+      } else { // The input array comes from the TS array
+        if (arrays instanceof Uint8Array) {
+          if ((data_width % 8 === 0) && (data_width / 8 > 1)) {
+            endian_mode = 1
+          }
+        }
+      }
+      return endian_mode
+    }
+
     // read init_file
     let init_data_array: string[] = []
+    let data_width = this.params.dataWidth
+    let isEndianMode = 0
     if (typeof this.MemInitFile === 'string') {
-      let init_data = ''
+      let init_data: string[] = []
       try {
         const data = fs.readFileSync(`${this.MemInitFile}`, 'utf8') // 将文件内容读取为UTF-8编码的字符串
-        console.log('read initial file successfully')
-        init_data = data
+        init_data = (data.split('\n')).map(item => (item.split('_').join('')).split(' ').join(''))// Convert the read file content into a string array by line, while removing '_' and spaces from the string
       } catch (err) {
         console.error(err)
       }
-      init_data_array = init_data.split('\n')
+      isEndianMode = EndianModeCheck(init_data, this.params.dataWidth)
+      if (isEndianMode) {
+        data_width = 8
+      }
+      init_data_array = getStringsAfterChars(init_data, data_width)
     } else {
-      init_data_array = this.MemInitFile
+      isEndianMode = EndianModeCheck(this.MemInitFile, this.params.dataWidth)
+      if (isEndianMode) {
+        data_width = 8
+      }
+      init_data_array = getStringsAfterChars(this.MemInitFile, data_width)
     }
 
     // calculate rom`s depth
-    const rom_depth = init_data_array.length
+    let rom_depth: number
+    if (isEndianMode) {
+      rom_depth = init_data_array.length / (this.params.dataWidth / 8)
+    } else {
+      rom_depth = init_data_array.length
+    }
 
     // define IO signals
     this.IOs = {
@@ -51,15 +145,32 @@ export class ROM extends Module {
     }
 
     // If the data bit width is uneven, fill the high bits of the data with 0
-    const arry_width = Math.ceil(this.params.dataWidth / 4)
+    const arry_width = Math.ceil(data_width / 4)
     const init_data_array_pad = init_data_array.map(array => array.padStart(arry_width, '0'))
 
     // construct output signal list
     const signalArray: string[] = []
-    let i = 0
-    for (const p of init_data_array_pad) {
-      signalArray.push(`${this.bitWidth(rom_depth)}'d${i}: data_out <= ${this.params.dataWidth}'h${p};\n`)
-      i = i + 1
+    if (isEndianMode) {
+      const byte_num = this.params.dataWidth / 8
+      let point = 0
+      for (let i = 0; i < rom_depth; i++) {
+        let rom_data = ''
+        if (this.params.endianness === 'little') {
+          for (let j = byte_num - 1; j >= 0; j--) {
+            rom_data += init_data_array_pad[point + j]
+          }
+        } else {
+          for (let j = 0; j < byte_num; j++) {
+            rom_data += init_data_array_pad[point + j]
+          }
+        }
+        point = point + byte_num
+        signalArray.push(`${this.bitWidth(rom_depth)}'d${i}: data_out <= ${this.params.dataWidth}'h${rom_data};\n`)
+      }
+    } else {
+      for (let n = 0; n < rom_depth; n++) {
+        signalArray.push(`${this.bitWidth(rom_depth)}'d${n}: data_out <= ${this.params.dataWidth}'h${init_data_array_pad[n]};\n`)
+      }
     }
 
     // define rom`s body
--- ts/test/modules/test_ROM.ts
+++ ts/test/modules/test_ROM.ts
@@ -1,23 +1,39 @@
 import { ROM } from 'tssv/lib/modules/ROM'
 import { writeFileSync } from 'fs'
 
-// Read hex file as ROM initial data
-const testMem1 = new ROM({ dataWidth: 16 }, 'ts/test/test_rom_init_file.txt')
+// Read hex file as ROM initial data, the data can be 0xbeefcafe, 0Xbeefcafe, 32'hbeefcafe, 'hbeefcafe, 'dbeefcafe and beefcafe(Without identifiers, default to decimal), where decimal numbers support negative values
+// rom_width:32 initial_data_width:32 initial_data_depth:6
+const testMem1 = new ROM({ dataWidth: 32 }, 'ts/test/test_rom_init_file.txt')
 try {
   writeFileSync(`sv-examples/test_ROM_output/${testMem1.name}.sv`, testMem1.writeSystemVerilog())
 } catch (err) {
   console.error(err)
 }
 
-// Convert the array to hexadecimal and read it as ROM initial data. Require initial information to be a hexadecimal number
-const test_array_dec: number [] = [1111, 5555, 43690, 65535]
-const test_array_hex: string [] = test_array_dec.map(decimal => decimal.toString(16).toUpperCase())
-// If you need an RCF file, simply add the output path of the RCF file in the parameter list
-const rcf_file_path: string = 'sv-examples/test_ROM_output'
-
-const testMem2 = new ROM({ name: 'myROM', dataWidth: 16 }, test_array_hex, rcf_file_path)
+// rom_width:32 initial_data_width:8 initial_data_depth:16 Binary
+const testMem2 = new ROM({ name: 'ROM_32_4_little_InitialFile', dataWidth: 32, endianness: 'big' }, 'ts/test/test_rom_init_file_32_4_binary.txt')
 try {
   writeFileSync(`sv-examples/test_ROM_output/${testMem2.name}.sv`, testMem2.writeSystemVerilog())
+} catch (err) {
+  console.error(err)
+}
+
+// If you need an RCF file, simply add the output path of the RCF file in the parameter list
+const rcf_file_path: string = 'sv-examples/test_ROM_output'
+// rom_width:16    array_data_width:less than 16    array_data_depth:4 decimal
+const test_array_dec: number [] = [-5, -256, 43690, 65535]
+const testMem3 = new ROM({ name: 'ROM_16_4_normal_array', dataWidth: 16, endianness: 'little' }, test_array_dec, rcf_file_path)
+try {
+  writeFileSync(`sv-examples/test_ROM_output/${testMem3.name}.sv`, testMem3.writeSystemVerilog())
+} catch (err) {
+  console.error(err)
+}
+
+// rom_width:16    array_data_width:less than 16    array_data_depth:4 decimal
+const test_unit8array_dec = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+const testMem4 = new ROM({ name: 'ROM_32_4_unit8_array', dataWidth: 32, endianness: 'little' }, test_unit8array_dec, rcf_file_path)
+try {
+  writeFileSync(`sv-examples/test_ROM_output/${testMem4.name}.sv`, testMem4.writeSystemVerilog())
 } catch (err) {
   console.error(err)
 }
